0af5f2b9ae992d2d3b6e5d500c6d86f8b541486a
beckstev <beckstev@web.de>
2017-05-05 13:13:02 +0200

    kleine korrekturen

--- steven/zettel2_aufgabe2_2.m
+++ steven/zettel2_aufgabe2_2.m
@@ -1,9 +1,10 @@
-x=single(0.8: 1e-6:1.2);
+x=single(0.8: 1e-4:1.2);
 
 format long
 pol =[1 -8 28 -56 70 -56 28 -8 1]
 
 %not 100% functional
+liste=[]
 for i = x
   beta=pol(1);
   for n = 2 : size(pol)(2)
@@ -22,11 +23,11 @@ figure
 plot(x,(1-x).^8,'r')
 figure
 plot(x,liste,'b')
-%xlim([0.75 1.25])
+xlim([0.75 1.25])
 %ylim([0 2.7*1e-6])
-%xlabel('x')
-%ylabel('y')
-%legend('(x-1)^8')
+xlabel('x')
+ylabel('y')
+legend('(x-1)^8','hornaschema')
 
 
 input('return')
