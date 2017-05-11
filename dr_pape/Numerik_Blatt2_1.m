e250b36bdd8298c4c385aac011b2a719aced7ed0
Sebastian Pape <SePa@gmx.de>
2017-05-11 15:55:50 +0200

    Hol den Vorschlag Hammer!

--- dr_pape/Numerik_Blatt2_1.m
+++ dr_pape/Numerik_Blatt2_1.m
@@ -4,31 +4,31 @@ format long
 % Approximation der Eulerschen Zahl nach der ersten Variante
 function approx_e = Approx_1(x, n)
   printf('Aufgabenteil a: ')
-  buffer = zeros(n);
-  for i = 1:n
-    buffer(i) = x^i/factorial(i); % Nach der gebenen Approximationsformel
+  buffer = sum(zeros(n + 1));
+  for i = 1:(n + 1)
+    buffer(i) = x**(i - 1) / factorial(i - 1); % Nach der gebenen Approximationsformel
   end
-  e = sum(sum(buffer))
+  e = sum(buffer)
 end
 
 % Approximation der Eulerschen Zahl in der zweiten Variante
 function approx_e_2 = Approx_2(x, n)
   printf('Aufgabenteil b: ')
-  buffer = zeros(n);
-  for i = 1:n
-    buffer(i) = -x^i/factorial(i);
+  buffer = sum(zeros(n + 1));
+  for i = 1:(n + 1)
+    buffer(i) = (abs(x))^(i - 1) / factorial(i - 1);
   end
-  e = 1/sum(sum(buffer))
+  e = 1/sum(buffer)
 end
 
 % Approximation der Eulerschen Zahl in der dirtten Variante
 function approx_e_3 = Approx_3(x, n)
   printf('Aufgabenteil c: ')
-  buffer = zeros(n);
-  for i = 1:n
-    buffer(i) = -1/factorial(i);
+  buffer = sum(zeros(n + 1));
+  for i = 1:(n + 1)
+    buffer(i) = (-1)**(i - 1) / factorial(i - 1);
   end
-  e = sum(sum(buffer))^(-x)
+  e = sum(buffer)**(abs(x))
 end
 
 % Schleife in der alle geforderten Funktionswerte abgelaufen werden
