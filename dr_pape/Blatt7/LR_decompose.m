4807b275d6726747c363beca94a60ba8da6d9ca6
Sebastian Pape <SePa@gmx.de>
2017-06-25 21:52:44 +0200

    schalala

--- dr_pape/Blatt7/LR_decompose.m
+++ dr_pape/Blatt7/LR_decompose.m
@@ -16,15 +16,10 @@ function LR = LR_decompose(A)
       
       g = n + 1;
       LR(i + 1, j:g) = LR(i + 1, j:g) - LR(j, j:g) ./ LR(j, j) .* LR(i + 1, j);
-      R(i + 1, j:g) = LR(i + 1, j:g) - LR(j, j:g) ./ LR(j, j) .* LR(i + 1, j);
       
       ### Berechnen der Elemente der L-Matrix ###
       LR(i + 1, j) = L_Element;
-      L(i + 1, j) = L_Element;
       
     end
-  end
-  L
-  R
-  
+  end  
 end
